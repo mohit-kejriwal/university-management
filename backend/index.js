312830917a178eb6621167fa77202be44eb1dce2
tecwithyash <yashwagh699@gmail.com>
2025-04-13 15:39:58 +0530

    backend code for registration and login!

---
+++ backend/index.js
@@ -0,0 +1,34 @@
+
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const cors = require('cors');
+const authRoutes = require('./routes/authRoutes');
+app.use('/api/auth', authRoutes);
+
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+app.use('/uploads', express.static('uploads'));
+app.use('/api/auth', authRoutes);
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => console.log("✅ MongoDB Connected"))
+.catch((err) => console.error("❌ DB Connection Error:", err));
+
+// Sample Route
+app.get('/', (req, res) => {
+  res.send('Backend working!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
